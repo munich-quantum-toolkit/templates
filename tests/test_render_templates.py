0a73d1f382d9e1fac187d54cf61825683b753a6e
Daniel Haag <121057143+denialhaag@users.noreply.github.com>
2025-09-12 01:18:59 +0200

    Fix mypy error instead of ignoring it
    Co-authored-by: Lukas Burgholzer <burgholzer@me.com>
    Signed-off-by: Daniel Haag <121057143+denialhaag@users.noreply.github.com>

--- tests/test_render_templates.py
+++ tests/test_render_templates.py
@@ -50,7 +50,7 @@ def _check_files(files: list[Path]) -> None:
 
 @pytest.mark.parametrize("project_type", ["c++-python", "pure-python"])
 @pytest.mark.parametrize("has_changelog_and_upgrade_guide", [True, False])
-def test_non_other(temp_dir: Path, project_type: str, has_changelog_and_upgrade_guide: bool) -> None:  # noqa: FBT001
+def test_non_other(temp_dir: Path, project_type: str, *, has_changelog_and_upgrade_guide: bool) -> None:
     """Test that templates for non-`other` projects are rendered correctly."""
     render_templates(
         name="Test",
