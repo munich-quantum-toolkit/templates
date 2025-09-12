dfc413954c05b233a2609440f335d679a185aa55
Daniel Haag <121057143+denialhaag@users.noreply.github.com>
2025-09-12 15:22:12 +0200

    ✅ Add tests (#67)
    ## Description
    
    This PR adds tests. In the process, this PR also restructures the script
    into a proper Python package.
    
    Fixes #11
    
    ## Checklist:
    
    - [x] The pull request only contains commits that are focused and
    relevant to this change.
    - [x] I have added appropriate tests that cover the new/changed
    functionality.
    - [x] ~I have updated the documentation to reflect these changes.~
    - [x] I have added entries to the changelog for any noteworthy
    additions, changes, fixes, or removals.
    - [x] ~I have added migration instructions to the upgrade guide (if
    needed).~
    - [x] The changes follow the project's style guidelines and introduce no
    new warnings.
    - [x] The changes are fully tested and pass the CI checks.
    - [x] I have reviewed my own code changes.
    
    ---------
    
    Signed-off-by: Daniel Haag <121057143+denialhaag@users.noreply.github.com>
    Co-authored-by: Lukas Burgholzer <burgholzer@me.com>

---
+++ src/mqt/templates/__main__.py
@@ -0,0 +1,143 @@
+# Copyright (c) 2023 - 2025 Chair for Design Automation, TUM
+# Copyright (c) 2025 Munich Quantum Software Company GmbH
+# All rights reserved.
+#
+# SPDX-License-Identifier: MIT
+#
+# Licensed under the MIT License
+
+"""Main entry point for rendering templates."""
+
+from __future__ import annotations
+
+import argparse
+
+from .render_templates import render_templates
+
+
+def _convert_to_bool(value: str) -> bool:
+    if value in {"True", "true"}:
+        return True
+    if value in {"False", "false"}:
+        return False
+    msg = f"Invalid boolean value: {value}"
+    raise ValueError(msg)
+
+
+def main() -> None:
+    """Parse arguments and render templates."""
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "--name",
+        type=str,
+        required=True,
+        help="Stylized name of the package (e.g., 'Core' or 'DDSIM')",
+    )
+    parser.add_argument(
+        "--organization",
+        type=str,
+        required=True,
+        help="Name of the repository's organization",
+    )
+    parser.add_argument(
+        "--project_type",
+        type=str,
+        choices=["c++-python", "pure-python", "other"],
+        required=True,
+        help="Type of the project",
+    )
+    parser.add_argument(
+        "--repository",
+        type=str,
+        required=True,
+        help="Name of the repository",
+    )
+    parser.add_argument(
+        "--has_changelog_and_upgrade_guide",
+        default=True,
+        type=_convert_to_bool,
+        help="Whether the repository has a changelog and upgrade guide",
+    )
+    parser.add_argument(
+        "--synchronize_contribution_guide",
+        default=True,
+        type=_convert_to_bool,
+        help="Whether to synchronize the contribution guide",
+    )
+    parser.add_argument(
+        "--synchronize_documentation_utilities",
+        default=True,
+        type=_convert_to_bool,
+        help="Whether to synchronize documentation utilities such as page.html and custom.css",
+    )
+    parser.add_argument(
+        "--synchronize_installation_guide",
+        default=True,
+        type=_convert_to_bool,
+        help="Whether to synchronize the installation guide",
+    )
+    parser.add_argument(
+        "--synchronize_issue_templates",
+        default=True,
+        type=_convert_to_bool,
+        help="Whether to synchronize the issue templates",
+    )
+    parser.add_argument(
+        "--synchronize_pull_request_template",
+        default=True,
+        type=_convert_to_bool,
+        help="Whether to synchronize the pull-request template",
+    )
+    parser.add_argument(
+        "--synchronize_release_drafter_template",
+        default=True,
+        type=_convert_to_bool,
+        help="Whether to synchronize the Release Drafter template",
+    )
+    parser.add_argument(
+        "--synchronize_renovate_config",
+        default=True,
+        type=_convert_to_bool,
+        help="Whether to synchronize the Renovate configuration",
+    )
+    parser.add_argument(
+        "--synchronize_security_policy",
+        default=True,
+        type=_convert_to_bool,
+        help="Whether to synchronize the security policy",
+    )
+    parser.add_argument(
+        "--synchronize_support_resources",
+        default=True,
+        type=_convert_to_bool,
+        help="Whether to synchronize the support resources",
+    )
+    parser.add_argument(
+        "--release_drafter_categories",
+        type=str,
+        default="",
+        help="Release Drafter categories as a JSON string. If empty, a reasonable default will be used",
+    )
+    args = parser.parse_args()
+
+    render_templates(
+        name=args.name,
+        organization=args.organization,
+        project_type=args.project_type,
+        repository=args.repository,
+        has_changelog_and_upgrade_guide=args.has_changelog_and_upgrade_guide,
+        synchronize_contribution_guide=args.synchronize_contribution_guide,
+        synchronize_documentation_utilities=args.synchronize_documentation_utilities,
+        synchronize_installation_guide=args.synchronize_installation_guide,
+        synchronize_issue_templates=args.synchronize_issue_templates,
+        synchronize_pull_request_template=args.synchronize_pull_request_template,
+        synchronize_release_drafter_template=args.synchronize_release_drafter_template,
+        synchronize_renovate_config=args.synchronize_renovate_config,
+        synchronize_security_policy=args.synchronize_security_policy,
+        synchronize_support_resources=args.synchronize_support_resources,
+        release_drafter_categories=args.release_drafter_categories,
+    )
+
+
+if __name__ == "__main__":
+    main()
---
+++ tests/test_render_templates.py
@@ -0,0 +1,123 @@
+# Copyright (c) 2023 - 2025 Chair for Design Automation, TUM
+# Copyright (c) 2025 Munich Quantum Software Company GmbH
+# All rights reserved.
+#
+# SPDX-License-Identifier: MIT
+#
+# Licensed under the MIT License
+
+"""Test the render_templates() function."""
+
+from __future__ import annotations
+
+import shutil
+from pathlib import Path
+from typing import TYPE_CHECKING
+
+import pre_commit.main
+import pytest
+
+from mqt.templates.render_templates import render_templates
+
+if TYPE_CHECKING:
+    from collections.abc import Generator
+
+    from pytest_mock import MockFixture
+
+
+@pytest.fixture
+def temp_dir(mocker: MockFixture) -> Generator[Path]:
+    """Create a temporary directory for testing.
+
+    Yields:
+        The path to the temporary directory.
+    """
+    temp_dir = Path(__file__).absolute().parent.parent / "temp"
+    temp_dir.mkdir(exist_ok=True)
+    mocker.patch("mqt.templates.render_templates.ROOT_DIR", temp_dir)
+    yield temp_dir
+    shutil.rmtree(temp_dir)
+
+
+def _check_files(files: list[Path]) -> None:
+    for file in files:
+        assert file.exists()
+        file_before = file.read_text()
+        pre_commit.main.main(["run", "--files", str(file)])
+        file_after = file.read_text()
+        assert file_before == file_after, f"File {file.name} was modified by pre-commit"
+
+
+@pytest.mark.parametrize("project_type", ["c++-python", "pure-python"])
+@pytest.mark.parametrize("has_changelog_and_upgrade_guide", [True, False])
+def test_non_other(temp_dir: Path, project_type: str, *, has_changelog_and_upgrade_guide: bool) -> None:
+    """Test that templates for non-`other` projects are rendered correctly."""
+    render_templates(
+        name="Test",
+        organization="munich-quantum-toolkit",
+        project_type=project_type,
+        repository="test",
+        has_changelog_and_upgrade_guide=has_changelog_and_upgrade_guide,
+        synchronize_contribution_guide=True,
+        synchronize_documentation_utilities=True,
+        synchronize_installation_guide=True,
+        synchronize_issue_templates=True,
+        synchronize_pull_request_template=True,
+        synchronize_release_drafter_template=True,
+        synchronize_renovate_config=True,
+        synchronize_security_policy=True,
+        synchronize_support_resources=True,
+        release_drafter_categories="",
+    )
+
+    files = [
+        temp_dir / ".github" / "CONTRIBUTING.md",
+        temp_dir / ".github" / "ISSUE_TEMPLATE" / "bug-report.yml",
+        temp_dir / ".github" / "ISSUE_TEMPLATE" / "feature-request.yml",
+        temp_dir / ".github" / "pull_request_template.md",
+        temp_dir / ".github" / "release-drafter.yml",
+        temp_dir / ".github" / "renovate.json5",
+        temp_dir / ".github" / "SECURITY.md",
+        temp_dir / ".github" / "SUPPORT.md",
+        temp_dir / "docs" / "_static" / "custom.css",
+        temp_dir / "docs" / "_templates" / "page.html",
+        temp_dir / "docs" / "contributing.md",
+        temp_dir / "docs" / "installation.md",
+        temp_dir / "docs" / "lit_header.bib",
+    ]
+    _check_files(files)
+
+
+def test_other(temp_dir: Path) -> None:
+    """Test that templates for `other` projects are rendered correctly."""
+    render_templates(
+        name="Test",
+        organization="munich-quantum-toolkit",
+        project_type="other",
+        repository="test",
+        has_changelog_and_upgrade_guide=True,
+        synchronize_contribution_guide=False,
+        synchronize_documentation_utilities=True,
+        synchronize_installation_guide=False,
+        synchronize_issue_templates=True,
+        synchronize_pull_request_template=True,
+        synchronize_release_drafter_template=True,
+        synchronize_renovate_config=True,
+        synchronize_security_policy=True,
+        synchronize_support_resources=True,
+        release_drafter_categories="",
+    )
+
+    files = [
+        temp_dir / ".github" / "ISSUE_TEMPLATE" / "bug-report.yml",
+        temp_dir / ".github" / "ISSUE_TEMPLATE" / "feature-request.yml",
+        temp_dir / ".github" / "pull_request_template.md",
+        temp_dir / ".github" / "release-drafter.yml",
+        temp_dir / ".github" / "renovate.json5",
+        temp_dir / ".github" / "SECURITY.md",
+        temp_dir / ".github" / "SUPPORT.md",
+        temp_dir / "docs" / "_static" / "custom.css",
+        temp_dir / "docs" / "_templates" / "page.html",
+        temp_dir / "docs" / "lit_header.bib",
+    ]
+    _check_files(files)
